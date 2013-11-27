12b7e4c50a9ff702974bd06426e446e572b56113
Tom Maher <tmaher@heroku.com>
2013-11-26 16:34:32 -0800

    helpers to generate payloads

--- lib/gitcredential.rb
+++ lib/gitcredential.rb
@@ -14,15 +14,30 @@ class Gitcredential
   attr_accessor :backend
   def initialize args = {}
     @valid_backends = ["osxkeychain", "winstore", "cache", "store", "default"]
+    @search_dft = {:proto => "https", :path => "/"}
     @backend = args[:backend] || Gitcredential.default_backend
 
     raise Exception "no such backend" unless @valid_backends.include?(@backend)
   end
 
   def cmd
-    "git-credential-#{backend}"
+    "git-credential-#{@backend}"
   end
-    
+
+  def get_payload search_data={}
+    u = @search_dft.merge search_data
+    <<-__EOS
+protocol=#{u[:proto]}
+host=#{u[:host]}
+path=#{u[:path]}
+username=#{u[:user]}
+__EOS
+  end
+
+  def set_payload set_data={}
+    get_payload(set_data) + "password=#{set_data[:pw]}\n"
+  end
+
   def get u
     nil
   end
